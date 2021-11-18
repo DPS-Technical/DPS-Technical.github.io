0c5b402fd649dd9684c80e0789d295b003ec32ed
DPS-Technical <andrew.swift@dpstechnical.co.uk>
2021-11-18 12:27:56 +0000

    final

--- pdfprint.js
+++ pdfprint.js
@@ -5,7 +5,7 @@ function print() {
     var myWindow = window.open('', '', 'width=200,height=100');
     var doc = myWindow.document;
     doc.open();
-    doc.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="stylesheetpopup.css"></head><body onload="window.print()">');
+    doc.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="stylesheet.css"><link rel="stylesheet" type="text/css" href="pdfback.css"></head><body onload="window.print()">');
     doc.write(divText);
     doc.write('</body></html>');
     doc.close();
