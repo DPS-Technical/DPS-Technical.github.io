567bbe59bfdb52c8b0c4bceb209521f0503ac4e7
DPS-Technical <andrew.swift@dpstechnical.co.uk>
2021-11-19 10:04:52 +0000

    print fix

--- pdfprint.js
+++ pdfprint.js
@@ -1,11 +1,12 @@
+
 function print() {
 
     var divText = document.getElementById("newcont").outerHTML;
     var divText = divText.replace(/['"]+/g, '');
     var hidWindow = window.open('','');
     var doc = hidWindow.document;
-    doc.open(left=1000,top=1000);
-    doc.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="stylesheet.css"><link rel="stylesheet" type="text/css" href="pdfback.css"></head><body onload="window.print()">');
+    doc.open();
+    doc.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="stylesheet.css"><link rel="stylesheet" type="text/css" href="pdfback.css"></head><body onload="window.print();" >');
     doc.write(divText);
     doc.write('</body></html>');
     doc.close();
